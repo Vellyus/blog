2f8b2044c6a45587c1137f3ae297dc720b8a477d
Vellyus <noisyboy026@gmail.com>
2023-05-25 17:46:38 +0200

    installed router + added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Admin } from "./pages/Admin";
+import { AdminBlog } from "./pages/AdminBlog";
+import { NotFound } from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home />, },
+  { path: "/admin", element: <Admin />, },
+  { path: "/admin/blog", element: <AdminBlog />, },
+  { path: "*", element: <NotFound /> }
+]);
+
 
 function App() {
-  return;
-}
+  return <RouterProvider router={ router } />;
+};
 
 export default App;
---
+++ src/pages/AdminBlog.js
@@ -0,0 +1,5 @@
+export function AdminBlog() {
+  return (
+    <h1>AdminBlog</h1>
+  );
+}
