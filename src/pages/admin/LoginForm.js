c4c1b71c2347397a756da48a1b5a3be3bbf5b264
Vellyus <noisyboy026@gmail.com>
2023-05-30 17:25:08 +0200

    added state to login- and registerForms

--- src/pages/admin/LoginForm.js
+++ src/pages/admin/LoginForm.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 export function LoginForm({ changeForm }) {
+  const [formState, setFormState] = useState();
+
+  const handleInputChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  };
+
   return (
     <>
       <h2>Sign In</h2>
       <form>
         <label htmlFor="email">
-          <input name="email" id="email" placeholder="E-mail" />
+          <input onChange={ handleInputChange } name="email" id="email" placeholder="E-mail" />
         </label>
 
         <label htmlFor="password">
-          <input name="password" id="password" placeholder="Password" />
+          <input onChange={ handleInputChange } name="password" id="password" placeholder="Password" />
         </label>
 
         <button type="submit">Submit</button>
@@ -17,7 +25,6 @@ export function LoginForm({ changeForm }) {
       <hr></hr>
 
       <button onClick={ changeForm }> I don't have an account</button>
-
     </>
   );
 }
--- src/pages/admin/RegisterForm.js
+++ src/pages/admin/RegisterForm.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 export function RegisterForm({ changeForm }) {
+  const [formState, setFormState] = useState();
+
+  const handleInputChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value });
+  };
+
   return (
     <>
       <h2>Sign Up</h2>
       <form>
         <label htmlFor="email">
-          <input name="email" id="email" placeholder="E-mail" />
+          <input onChange={ handleInputChange } name="email" id="email" placeholder="E-mail" />
         </label>
 
         <label htmlFor="password">
-          <input name="password" id="password" placeholder="Password" />
+          <input onChange={ handleInputChange } name="password" id="password" placeholder="Password" />
         </label>
 
         <button type="submit">Submit</button>
@@ -17,7 +25,6 @@ export function RegisterForm({ changeForm }) {
       <hr></hr>
 
       <button onClick={ changeForm }>I already have an account</button>
-
     </>
   );
 }
