d6ac896dfc1744cc046b56fa2a4293c0349acb5f
Vellyus <noisyboy026@gmail.com>
2023-06-02 16:27:23 +0200

    moved firebaseConfig to constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,11 @@
 export const dbUrl = "https://blog-22bf7-default-rtdb.europe-west1.firebasedatabase.app/";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCOahx1HD1Ns2_Z2verICVrzBarVj0P-Gc",
+  authDomain: "blog-22bf7.firebaseapp.com",
+  databaseURL: dbUrl,
+  projectId: "blog-22bf7",
+  storageBucket: "blog-22bf7.appspot.com",
+  messagingSenderId: "389016441869",
+  appId: "1:389016441869:web:b8f2af9c1a6d88beaf3144"
+};
--- src/service/authService.js
+++ src/service/authService.js
@@ -1,17 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { dbUrl } from '../constant';
+import { firebaseConfig } from "../constant";
 import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCOahx1HD1Ns2_Z2verICVrzBarVj0P-Gc",
-  authDomain: "blog-22bf7.firebaseapp.com",
-  databaseURL: dbUrl,
-  projectId: "blog-22bf7",
-  storageBucket: "blog-22bf7.appspot.com",
-  messagingSenderId: "389016441869",
-  appId: "1:389016441869:web:b8f2af9c1a6d88beaf3144"
-};
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
