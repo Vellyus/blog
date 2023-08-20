023df4cd310d73023f1043612e9cf82d701239e9
Vellyus <noisyboy026@gmail.com>
2023-08-20 18:54:02 +0200

    added 404 page to adminRoutes, but the blogRoutes one does not work

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,7 +1,9 @@
 import { Admin } from "../pages/admin/Admin";
 import { AdminBlog } from "../pages/admin/AdminBlog";
+import { NotFound } from "../pages/blog/NotFound";
 
 export const adminRoutes = [
   { path: "/admin", element: <Admin />, },
   { path: "/admin/blog", element: <AdminBlog />, },
+  { path: "*", element: <NotFound /> }
 ];
--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -5,5 +5,5 @@ import { Article } from "../pages/blog/Article";
 export const blogRoutes = [
   { path: "/", element: <Home />, },
   { path: "/:id", element: <Article />, },
-  { path: "*", element: <NotFound /> }
+  { path: "*", element: <NotFound /> }   // 404 page is not showing up, because the router is handling it as a dynamic segment url, but it can't load an Article element
 ];
