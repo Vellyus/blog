83e6d1a94177fd60a33de6ee5d6dd320e201a6aa
Vellyus <noisyboy026@gmail.com>
2023-11-04 12:17:41 +0100

    test app with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import { RouterProvider } from "react-router-dom";
 import { getData } from './service/blogService';
 import { adminRoutes } from './routes/adminRoutes';
 import { blogRoutes } from './routes/blogRoutes';
 import { LoginProvider } from './LoginContext';
 
-const router = createBrowserRouter([
+// const router = createBrowserRouter([
+//   { path: "/", children: blogRoutes },
+//   { path: "/admin", children: adminRoutes }
+// ]);
+
+const router = HashRouter([
   { path: "/", children: blogRoutes },
   { path: "/admin", children: adminRoutes }
 ]);
 
-console.log(getData());
+// console.log(getData());
 
 function App() {
   return (
