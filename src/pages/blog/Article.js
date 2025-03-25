de45640d27fc339778dec0a022beb7fd6cc50652
Vellyus <noisyboy026@gmail.com>
2025-03-25 11:33:40 +0100

    changed some styles

--- src/pages/blog/Article.js
+++ src/pages/blog/Article.js
@@ -1,41 +1,43 @@
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { getData } from "../../service/blogService";
-import { dbUrl } from "../../constant";
-import { useNavigate } from "react-router-dom";
-import BackToMain from '../../components/BackToMain';
-import ArticleTitle from '../../components/ArticleTitle';
-import ArticleLead from "../../components/ArticleLead";
-import ArticleBody from "../../components/ArticleBody";
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { getData } from "../../service/blogService"
+import { dbUrl } from "../../constant"
+import { useNavigate } from "react-router-dom"
+import BackToMain from '../../components/BackToMain'
+import ArticleTitle from '../../components/ArticleTitle'
+import ArticleLead from "../../components/ArticleLead"
+import ArticleBody from "../../components/ArticleBody"
 
 export function Article() {
-  const id = useParams().id;
-  const navigate = useNavigate();
+  const id = useParams().id
+  const navigate = useNavigate()
 
-  const [blogPost, setBlogPost] = useState();
+  const [blogPost, setBlogPost] = useState()
 
   useEffect(() => {
-    getData(dbUrl).then(data => setBlogPost(data));
-  }, [blogPost]);
+    getData(dbUrl).then(data => setBlogPost(data))
+  }, [blogPost])
 
   const handleNavToMainPage = () => {
-    navigate(`/`, { replace: true });
-  };
+    navigate(`/`, { replace: true })
+  }
 
   return (
     <>
-      {
-        blogPost &&
-        <article>
-          { blogPost[id].imageURL !== "No image uploaded" ? <img className="bigImg" alt="article" src={ `${ blogPost[id].imageURL }` }></img> : null }
-          <ArticleTitle title={ blogPost[id].title } />
-          <ArticleLead lead={ blogPost[id].lead } />
-          <ArticleBody body={ blogPost[id].body } />
-          {/* <p>{ blogPost[id].lead }</p> */ }
-          {/* <p>{ blogPost[id].body }</p> */ }
-          <BackToMain handleNavToMainPage={ handleNavToMainPage } />
-        </article>
-      }
+      <main>
+        {
+          blogPost &&
+          <article>
+            {blogPost[id].imageURL !== "No image uploaded" ? <img className="bigImg" alt="article" src={`${ blogPost[id].imageURL }`}></img> : null}
+            <ArticleTitle title={blogPost[id].title} />
+            <ArticleLead lead={blogPost[id].lead} />
+            <ArticleBody body={blogPost[id].body} />
+            {/* <p>{ blogPost[id].lead }</p> */}
+            {/* <p>{ blogPost[id].body }</p> */}
+            <BackToMain handleNavToMainPage={handleNavToMainPage} />
+          </article>
+        }
+      </main>
     </>
-  );
+  )
 }
