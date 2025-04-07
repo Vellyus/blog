864e3b56d0631ae22f5921b7067689c13b9d1ac1
Vellyus <noisyboy026@gmail.com>
2025-04-07 14:56:06 +0200

    added back the console logs

--- src/service/blogService.js
+++ src/service/blogService.js
@@ -1,26 +1,25 @@
-import { getDatabase, ref, child, get, set } from "firebase/database";
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "../constant";
+import { getDatabase, ref, child, get, set } from "firebase/database"
+import { initializeApp } from "firebase/app"
+import { firebaseConfig } from "../constant"
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-const dbRef = ref(getDatabase());
+const app = initializeApp(firebaseConfig)
+const db = getDatabase()
+const dbRef = ref(getDatabase())
 
 export function getData() {
   return get(child(dbRef, "/")).then((snapshot) => {
     if (snapshot.exists()) {
-      // console.log(snapshot.val());
-      return snapshot.val();
+      return snapshot.val()
     } else {
-      console.log("No data available");
+      console.log("No data available")
     }
   }).catch((error) => {
-    console.error(error);
-  });
+    console.error(error)
+  })
 }
 
 export function addOrEditBlogPost(id, image, title, lead, body, imageURL) {
-  const reference = ref(db, id);
+  const reference = ref(db, id)
   return set(reference, {
     image: image,
     title: title,
@@ -28,10 +27,10 @@ export function addOrEditBlogPost(id, image, title, lead, body, imageURL) {
     body: body,
     imageURL: imageURL,
     id: id
-  });
+  })
 }
 
 export function removeBlogPost(id) {
-  const reference = ref(db, id);
-  set(reference, null);
+  const reference = ref(db, id)
+  set(reference, null)
 }
